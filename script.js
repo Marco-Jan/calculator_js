42967dad4e0aad7738ed3a866e9b2104d365eab1
Iron <marco.jan@gmx.de>
2023-06-13 09:42:26 +0200

    init commit

--- script.js
+++ script.js
@@ -1,84 +1,40 @@
-const display = document.querySelector('#result');
-let displayValue = '0';
-// Input
-function input_btn(value) {
-  let resultElement = document.getElementById('result');
-  let currentContent = resultElement.innerHTML;
-  let newContent = currentContent + value;
-  resultElement.innerHTML = newContent;
-}
-
-// Ergebnis berechnen
-/* function calculateResult() {
-  let containerElement = document.getElementById('result');
-  let containerContent = containerElement.innerHTML;
-  let result = eval(containerContent);
-  containerElement.innerHTML = result;
-} */
-
-// Zurücksetzen
-function reset() {
-  let containerElement = document.getElementById('result');
-  containerElement.innerHTML = '';
-}
-
+const display = document.querySelector('#reset');
 const calculator = document.getElementById('calculator');
+let displayValue = '0';
+
+
+
+function input_btn(value) {
+    let resultElement = document.getElementById('#result');
+    let currentContent = resultElement.innerHTML;
+    let newContent = currentContent + value;
+    resultElement.innerHTML = newContent;
+}
 
 calculator.addEventListener('click', (event) => {
-  let key = event.target;
-  let action = key.dataset.action;
-  let keyValue = key.dataset.value;
-  if (!action) {
-   /*  let value = key.dataset.value;
-    if (value) {
-      input_btn(value);
-    } */
-    handleInputNum(keyValue);
-    updateDisplay();
-  }
+    let key = event.target;
+    let action = key.dataset.action;
+    let keyValue = key.dataset.value;
+    if (!action) {
+        /*  let value = key.dataset.value;
+         if (value) {
+           input_btn(value);
+         } */
+        handleInputNum(keyValue);
+        updateDisplay();
+    }
 
-
-  if(action === '+' || action === '-' || action === '*'  || action === '/' ){
-    
-    z1 = displayValue
-  }
 });
-
 function updateDisplay() {
-  display.textContent = displayValue;
+    display.textContent = displayValue;
 }
-
-function calculate(z1, z2, operator) {
-  if(operator === '+') {
-    return z1 + z2;
-  }
-  if(operator === '-') {
-    return z1 - z2;
-  }
-  if(operator === '*') {
-    return z1 * z2;
-  }
-  if(operator === '/') {
-    return z1 / z2;
-  }
-  return z2;
-}
-
 
 function handleInputNum(key) {
-  displayValue = displayValue == 0 ? key : displayValue + key;
-  /* if(displayValue == 0) {
-    displayValue = key;
-  }else {
-    displayValue = displayValue + key
-  } */
+    displayValue = displayValue == 0 ? key : displayValue + key;
+    /* if(displayValue == 0) {
+      displayValue = key;
+    }else {
+      displayValue = displayValue + key
+    } */
 }
-
-
-function handleInputoperator(){
-
-
-}
-
 updateDisplay();
-
