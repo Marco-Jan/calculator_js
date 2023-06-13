6c3d5ac2f3c8022e75effc53df24f3219aed5b85
Iron <marco.jan@gmx.de>
2023-06-13 09:24:50 +0200

    init commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const display = document.querySelector('#result');
+let displayValue = '0';
 // Input
 function input_btn(value) {
   let resultElement = document.getElementById('result');
@@ -7,12 +9,12 @@ function input_btn(value) {
 }
 
 // Ergebnis berechnen
-function calculateResult() {
+/* function calculateResult() {
   let containerElement = document.getElementById('result');
   let containerContent = containerElement.innerHTML;
   let result = eval(containerContent);
   containerElement.innerHTML = result;
-}
+} */
 
 // Zurücksetzen
 function reset() {
@@ -25,11 +27,58 @@ const calculator = document.getElementById('calculator');
 calculator.addEventListener('click', (event) => {
   let key = event.target;
   let action = key.dataset.action;
-
+  let keyValue = key.dataset.value;
   if (!action) {
-    let value = key.dataset.value;
+   /*  let value = key.dataset.value;
     if (value) {
       input_btn(value);
-    }
-  } 
+    } */
+    handleInputNum(keyValue);
+    updateDisplay();
+  }
+
+
+  if(action === '+' || action === '-' || action === '*'  || action === '/' ){
+    
+    z1 = displayValue
+  }
 });
+
+function updateDisplay() {
+  display.textContent = displayValue;
+}
+
+function calculate(z1, z2, operator) {
+  if(operator === '+') {
+    return z1 + z2;
+  }
+  if(operator === '-') {
+    return z1 - z2;
+  }
+  if(operator === '*') {
+    return z1 * z2;
+  }
+  if(operator === '/') {
+    return z1 / z2;
+  }
+  return z2;
+}
+
+
+function handleInputNum(key) {
+  displayValue = displayValue == 0 ? key : displayValue + key;
+  /* if(displayValue == 0) {
+    displayValue = key;
+  }else {
+    displayValue = displayValue + key
+  } */
+}
+
+
+function handleInputoperator(){
+
+
+}
+
+updateDisplay();
+
